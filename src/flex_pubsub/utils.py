f2f52117de50d854241afab633e7c35c39c3916b
Ali Avani <ali.avani@outlook.com>
2024-12-13 22:04:08 +0330

    bump version to 0.0.6 and improve subscription validation logic

--- src/flex_pubsub/utils.py
+++ src/flex_pubsub/utils.py
@@ -5,7 +5,9 @@ from .app_settings import app_settings
 
 
 def are_subscriptions_valid(subscriptions: List[StrEnum]):
-    return any(i.value in app_settings.SUBSCRIPTIONS for i in subscriptions)
+    if app_settings.SUBSCRIPTIONS:
+        return any(i in app_settings.SUBSCRIPTIONS for i in subscriptions)
+    return True
 
 
 def get_all_subclasses(klass):
